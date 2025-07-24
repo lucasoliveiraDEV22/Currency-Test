e33a0095f286eef8ec1445a090143cd557477652
lucasoliveiraDEV22 <lucaszrones@gmail.com>
2025-07-23 21:39:16 -0300

    new-commit

--- cypress.config.js
+++ cypress.config.js
@@ -8,7 +8,8 @@ module.exports = defineConfig({
     baseUrl: 'https://currency-new-converter.netlify.app/',
     screenshotOnRunFailure: false, // Desabilitar screenshots para não atrapalhar o log
     watchForFileChanges: true, // Habilita a observação de alterações no arquivo
-    video: true // Habilita a gravação de vídeos dos testes
-    // specPattern: 'cypress/e2e/**/*.cy.{js,ts}' // Inclui os testes .cy.ts ou .spec.ts
+    experimentalSessionAndOrigin: true, // Habilita o suporte experimental para sessões e origens
+    video: true, // Habilita a gravação de vídeos dos testes
+    specPattern: 'cypress/e2e/**/*.cy.{js,ts}' // Inclui os testes .cy.ts ou .spec.ts
   }
 });
--- cypress/e2e/currency-page.cy.js
+++ cypress/e2e/currency-page.cy.js
@@ -24,81 +24,61 @@ describe('Automatizando  ', () => {
   //Verificando o input de valor
   it('02-Verificar input e valor convertido de R$ para US$', () => {
     cy.get('input')
-      .debug()
       .should('have.attr', 'placeholder', 'R$ 10.000,00') //verificando se o input está visível
       .type('100'); //digitando um valor no input
     cy.get('button').should('have.attr', 'id', 'converter-Button').click(); //verificando se o botão de conversão está visível
   });
 
-  it('03-Converter de Euro para Reais', () => {
+  it.only('03-Converter de Euro para Reais', () => {
     //Fazendo asserção para verificar a primeira label
     cy.get('label')
       .eq(0)
-      .debug()
       .should('contain.text', 'Converter de')
       .children('select') //selecionando o select dentro da label
       .select('R$ Real Brasileiro');
     //Fazendo asserção para verificar a segunda label
-    cy.intercept(
-      'https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL'
-    ).as('currencyData');
+
     cy.get('label')
       .eq(1)
       .should('contain.text', 'Converter para')
       .children('select');
 
     cy.get('label')
-      .debug()
       .should('be.visible')
       .contains('Converter para')
       .children('select')
       .should('contain.text', '€ Euro')
       .select('€ Euro'); //selecionando o Euro
-    cy.wait('@currencyData').its('response.statusCode').should('eq', 200); //esperando a requisição ser concluída
     cy.get('input')
-      .debug()
       .should('have.attr', 'placeholder', 'R$ 10.000,00') //verificando se o input está visível
       .type('100'); //digitando um valor no input
-    cy.intercept(
-      'https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL'
-    ).as('buttonData');
-    cy.get('button').should('have.attr', 'id', 'converter-Button').click(); //verificando se o botão de conversão está visível
-    cy.wait('@buttonData').its('response.statusCode').should('eq', 200); //esperando a requisição ser concluída
+    cy.get('button').should('have.attr', 'id', 'converter-Button').click(); //verificando se o botão de
   });
 
-  it('04-Converter de Bitcoin para Reais', () => {
+  it.only('04-Converter de Bitcoin para Reais', () => {
     //Fazendo asserção para verificar a primeira label
     cy.get('label')
       .eq(0)
-      .debug()
       .should('contain.text', 'Converter de')
       .children('select') //selecionando o select dentro da label
       .select('R$ Real Brasileiro');
     //Fazendo asserção para verificar a segunda label
-    cy.intercept(
-      'https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL'
-    ).as('currencyData');
     cy.get('label')
-      .debug()
       .eq(1)
       .should('contain.text', 'Converter para')
       .children('select');
 
     cy.get('label')
-      .debug()
       .should('be.visible')
       .contains('Converter para')
       .children('select')
       .should('contain.text', 'Bitcoin')
       .select('Bitcoin'); //selecionando o Euro
-    cy.wait('@currencyData').its('response.statusCode').should('eq', 200); //esperando a requisição ser concluída
+
     cy.get('input')
       .should('have.attr', 'placeholder', 'R$ 10.000,00') //verificando se o input está visível
       .type('500'); //digitando um valor no input
-    cy.intercept(
-      'https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL'
-    ).as('buttonData');
+
     cy.get('button').should('have.attr', 'id', 'converter-Button').click(); //verificando se o botão de conversão está visível
-    cy.wait('@buttonData').its('response.statusCode').should('eq', 200); //esperando a requisição ser concluída
   });
 });
